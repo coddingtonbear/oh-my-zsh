6611985c90e686115c244577ebed456247dc1c13
Adam Coddington <adam@parthenonsoftware.com>
2012-04-25 14:41:35 -0700

    Adding functions for gathering the current mercurial branch to the mercurial plugin.  Updating latestrevision theme to utilize such information.

--- plugins/mercurial/mercurial.plugin.zsh
+++ plugins/mercurial/mercurial.plugin.zsh
@@ -12,3 +12,29 @@ alias hgp='hg push'
 alias hgs='hg status'
 # this is the 'git commit --amend' equivalent
 alias hgca='hg qimport -r tip ; hg qrefresh -e ; hg qfinish tip'
+
+function set_hg_branch_info() {
+    HG_BRANCH=""
+    # Tries to use FastRoot first -- https://bitbucket.org/yaniv_aknin/fast_hg_root
+    root=$(fast_hg_root)
+    if [ "${?}" -eq "0" ] ; then
+        HG_BRANCH=$(cat ${root}/.hg/branch 2> /dev/null) || return
+        return false
+    elif [ "${?}" -eq "127" ]; then
+        HG_BRANCH=$(hg branch 2> /dev/null) || return
+        return false
+    fi
+    return true
+}
+
+function hg_prompt_info() {
+    if [ -n "$HG_BRANCH" ]; then
+        echo "$ZSH_THEME_GIT_PROMPT_PREFIX%{$HG_BRANCH%}$ZSH_THEME_GIT_PROMPT_SUFFIX"
+    else
+        echo ""
+    fi
+}
+
+PERIOD=60
+add-zsh-hook chpwd set_hg_branch_info
+add-zsh-hook periodic set_hg_branch_info
