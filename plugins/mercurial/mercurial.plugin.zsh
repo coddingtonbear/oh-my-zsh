1fa41f7bfe60c2ae823cd2d4a1fd50be20f70ef9
Adam Coddington <adam@parthenonsoftware.com>
2012-04-26 09:30:15 -0700

    Minor refactoring, updating query to calculate upon only the default sheet.

--- plugins/mercurial/mercurial.plugin.zsh
+++ plugins/mercurial/mercurial.plugin.zsh
@@ -16,12 +16,12 @@ alias hgca='hg qimport -r tip ; hg qrefresh -e ; hg qfinish tip'
 function set_hg_branch_info() {
     HG_BRANCH=""
     # Tries to use FastRoot first -- https://bitbucket.org/yaniv_aknin/fast_hg_root
-    root=$(fast_hg_root 2> /dev/null)
-    retcode="${?}"
-    if [ "${retcode}" -eq "0" ] ; then
-        HG_BRANCH=$(cat ${root}/.hg/branch 2> /dev/null) || return
+    REPO_ROOT=$(fast_hg_root 2> /dev/null)
+    RETCODE="${?}"
+    if [ "${RETCODE}" -eq "0" ] ; then
+        HG_BRANCH=$(cat ${REPO_ROOT}/.hg/branch 2> /dev/null) || return
         return true
-    elif [ "${retcode}" -eq "127" ]; then
+    elif [ "${RETCODE}" -eq "127" ]; then
         HG_BRANCH=$(hg branch 2> /dev/null) || return
         return true
     fi
--- plugins/timebook/timebook.plugin.zsh
+++ plugins/timebook/timebook.plugin.zsh
@@ -1,5 +1,5 @@
 function current_timebook_status(){
-    ref=$(sqlite3 -nullvalue 'NULL' ~/.config/timebook/sheets.db "SELECT end_time, description, ROUND((strftime('%s', 'now') - start_time) / CAST(3600 AS FLOAT), 2) FROM entry WHERE id = (select max(id) from entry);" 2> /dev/null) || return
+    ref=$(sqlite3 -nullvalue 'NULL' ~/.config/timebook/sheets.db "SELECT end_time, description, ROUND((strftime('%s', 'now') - start_time) / CAST(3600 AS FLOAT), 2) FROM entry WHERE id = (select max(id) from entry where sheet = 'default');" 2> /dev/null) || return
     end_time=$ref[(ws:|:)1];
     doc_status=$ref[(ws:|:)2];
     hours=$ref[(ws:|:)3];
