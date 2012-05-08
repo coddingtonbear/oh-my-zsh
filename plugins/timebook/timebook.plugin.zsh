c536f428214669bf3cdf10262d40b427edb8d42e
Adam Coddington <adam@parthenonsoftware.com>
2012-05-07 20:06:31 -0700

    Cleaning up SQL query.  Updating prompt to utilize more variables.

--- plugins/timebook/timebook.plugin.zsh
+++ plugins/timebook/timebook.plugin.zsh
@@ -1,15 +1,48 @@
 function current_timebook_status(){
-    ref=$(sqlite3 -nullvalue 'NULL' ~/.config/timebook/sheets.db "SELECT end_time, description, ROUND((strftime('%s', 'now') - start_time) / CAST(3600 AS FLOAT), 2) FROM entry WHERE id = (select max(id) from entry where sheet = 'default');" 2> /dev/null) || return
+    ref=$(sqlite3 -nullvalue 'NULL' ~/.config/timebook/sheets.db "
+        SELECT
+            end_time, description, ROUND((strftime('%s', 'now') - start_time) / CAST(3600 AS FLOAT), 2),
+            em_ticket.value,
+            em_billable.value,
+            Null
+        FROM entry e
+        LEFT OUTER JOIN entry_meta em_ticket ON
+            e.id = em_ticket.entry_id
+            and
+            em_ticket.key = 'ticket_number'
+        LEFT OUTER JOIN entry_meta em_billable ON
+            e.id = em_billable.entry_id
+            and
+            em_billable.key = 'billable'
+        WHERE id = (
+            SELECT max(id) 
+            FROM entry 
+            WHERE sheet = 'default'
+        );
+        " 2> /dev/null) || return
+    ref=${ref// /};
     end_time=$ref[(ws:|:)1];
     doc_status=$ref[(ws:|:)2];
     hours=$ref[(ws:|:)3];
+    ticket=$ref[(ws:|:)4];
+    billable_string=$ref[(ws:|:)5];
+    if [[ $billable_string == "yes" ]]; then
+        billable="";
+    else;
+        billable="(¬$) ";
+    fi;
     if [[ $end_time == "NULL" ]]; then
-        echo "[${doc_status} ${hours}h] "
-    fi
+        echo "[#${ticket}, \"${doc_status}\" ${billable}${hours}h] "
+    fi;
 }
 
 function current_timesheet_balance(){
-    ref=$(sqlite3 -nullvalue 'NULL' ~/.config/timebook/sheets.db "SELECT ROUND(SUM(COALESCE(end_time, strftime('%s', 'now')) - start_time) / CAST(3600 AS FLOAT), 1) from entry where sheet = 'default' and entry.start_time > strftime('%s', strftime('%Y-%m-%d', 'now', 'localtime'), 'utc')" 2> /dev/null) || return
+    ref=$(sqlite3 -nullvalue 'NULL' ~/.config/timebook/sheets.db "
+        SELECT
+            ROUND(SUM(COALESCE(end_time, strftime('%s', 'now')) - start_time) / CAST(3600 AS FLOAT), 1)
+        FROM entry
+        WHERE sheet = 'default' and entry.start_time > strftime('%s', strftime('%Y-%m-%d', 'now', 'localtime'), 'utc')
+        " 2> /dev/null) || return
     curr_hours=$ref[(ws:|:)1];
     if [[ $curr_hours -lt 7 ]]; then
         color=$FG[125]
